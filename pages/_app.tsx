57aaed8c5a26b20db38e9cf25f18fb1dac985a67
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-06-16 15:26:13 +0300

    init auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
+import {useState, useEffect} from 'react'
+import {API, Auth, Hub} from 'aws-amplify'
 import '../styles/globals.css'
+import '../configureAmplify'
+import {createDentist} from "../graphql/mutations";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  const [signedInUser, setSignedInUser] = useState(false)
+
+  useEffect(() => {
+    authListener()
+  })
+
+  async function authListener() {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return setSignedInUser(true)
+        case 'signOut':
+          return setSignedInUser(false)
+      }
+    })
+    try {
+      await Auth.currentAuthenticatedUser()
+      // console.log(user.attributes.email)
+      setSignedInUser(true)
+    } catch (err) {
+    }
+  }
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
