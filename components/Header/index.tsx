83fbb9b5de9c819837857d3095370d6ab0eff19d
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-06-21 18:22:44 +0300

    profile, account, images

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,32 +7,40 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Link from "next/link";
 import {Menu, MenuItem} from "@material-ui/core";
 import PopupState, {bindMenu, bindTrigger} from 'material-ui-popup-state';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Auth, Hub} from "aws-amplify";
 import Router from "next/router";
 
-export default function Header() {
-  const [signedInUser, setSignedInUser] = useState(false)
+const initState: any = {
+  signedInUser: false,
+  currentUser: []
+};
 
-  useEffect(() => {
-    authListener()
-  })
-  async function authListener() {
+class Header extends React.Component {
+  state = initState
+
+  async componentDidMount() {
+    await this.authListener();
+  }
+
+  async authListener() {
     Hub.listen('auth', (data) => {
       switch (data.payload.event) {
         case 'signIn':
-          return setSignedInUser(true)
+          return this.setState({signedInUser: true})
         case 'signOut':
-          return setSignedInUser(false)
+          return this.setState({signedInUser: false})
       }
     })
     try {
-      await Auth.currentAuthenticatedUser()
-      setSignedInUser(true)
-    } catch (err) {}
+      const currentUser = await Auth.currentAuthenticatedUser()
+      this.setState({currentUser})
+      this.setState({signedInUser: true})
+    } catch (err) {
+    }
   }
 
-  async function signOut() {
+  async signOut() {
     try {
       await Auth.signOut();
       await Router.replace('/')
@@ -40,53 +48,57 @@ export default function Header() {
       console.log('error signing out: ', error);
     }
   }
-
-  return (
-    <AppBar position="static" style={{background: '#0d9da6'}}>
-      <Toolbar style={{justifyContent: 'space-between', display: 'flex'}}>
-        <div style={{alignItems: 'center', display: 'flex'}}>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <PopupState variant="popover" popupId="demo-popup-menu">
-              {(popupState) => (
-                <>
-                  <MenuIcon {...bindTrigger(popupState)}/>
-                  <Menu {...bindMenu(popupState)}>
-                    <MenuItem onClick={popupState.close}>
-                      <Link href={"../../dentist/account/"}>Account</Link>
-                    </MenuItem>
-                    <MenuItem onClick={popupState.close}>
-                      <Link href={"../../dentist/profile/"}><a>Profile</a></Link>
-                    </MenuItem>
-                    <MenuItem onClick={popupState.close}>
-                      <Link href={"../../dentist/gallery/"}><a>Gallery</a></Link>
-                    </MenuItem>
-                    <MenuItem onClick={popupState.close}>
-                      <Link href={"../../dentist/subscriptions/"}><a>Settings</a></Link>
-                    </MenuItem>
-                  </Menu>
-                </>
-              )}
-            </PopupState>
-          </IconButton>
-          <Typography variant="h6">
-            Dentist App
-          </Typography>
-        </div>
-        <Link href={"../../../search"}>
-          <Button color="inherit">Search</Button>
-        </Link>
-        {!signedInUser && (
-          <div>
-            <Link href={"../../../login"}>
-              <Button color="inherit">Login</Button>
-            </Link>
-            <Link href={"../../../register"}>
-              <Button color="inherit">Register</Button>
-            </Link>
+  render() {
+    return (
+      <AppBar position="static" style={{background: '#0d9da6'}}>
+        <Toolbar style={{justifyContent: 'space-between', display: 'flex'}}>
+          <div style={{alignItems: 'center', display: 'flex'}}>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              {this.state.signedInUser && <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <>
+                    <MenuIcon {...bindTrigger(popupState)}/>
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem onClick={popupState.close}>
+                        <Link href={"../../dentist/account/" + this.state.currentUser.username}>Account</Link>
+                      </MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <Link href={"../../dentist/profile/" + this.state.currentUser.username}><a>Profile</a></Link>
+                      </MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <Link href={"../../dentist/gallery/" + this.state.currentUser.username}><a>Gallery</a></Link>
+                      </MenuItem>
+                      <MenuItem onClick={popupState.close}>
+                        <Link
+                          href={"../../dentist/subscriptions/" + this.state.currentUser.username}><a>Settings</a></Link>
+                      </MenuItem>
+                    </Menu>
+                  </>
+                )}
+              </PopupState>}
+            </IconButton>
+            <Typography variant="h6">
+              Dentist App
+            </Typography>
           </div>
-        )}
-        {signedInUser && <Button onClick={signOut} color="inherit">Logout</Button>}
-      </Toolbar>
-    </AppBar>
-  );
+          <Link href={"../../../search"}>
+            <Button color="inherit">Search</Button>
+          </Link>
+          {!this.state.signedInUser && (
+            <div>
+              <Link href={"../../../login"}>
+                <Button color="inherit">Login</Button>
+              </Link>
+              <Link href={"../../../register"}>
+                <Button color="inherit">Register</Button>
+              </Link>
+            </div>
+          )}
+          {this.state.signedInUser && <Button onClick={this.signOut} color="inherit">Logout</Button>}
+        </Toolbar>
+      </AppBar>
+    )
+  }
 }
+
+export default Header
