a85870ca35b6b609231568ba2e75276d09854ef7
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-06-24 16:08:01 +0300

    build problem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import App from "next/app";
 import React from "react";
+import {ApolloProvider} from '@apollo/client/react';
 import {Elements} from "@stripe/react-stripe-js";
 import {loadStripe} from '@stripe/stripe-js';
+import withApollo from "../lib/withApollo";
 import '../styles/globals.css'
 import '../configureAmplify'
 import {QueryClientProvider, QueryClient} from "react-query";
@@ -12,13 +14,15 @@ class MyApp extends App<any> {
     const queryClient = new QueryClient()
     const stripePromise = loadStripe("pk_test_51ISNerEE2uETn4H3tqYplaINZlDwEa8CM7ohtp7YbxF7KJnUysRK8rjssQxSbruzaL6uJz4951GhHoZcHM3qWcLN00drEQTQ5M");
     return (
-      <QueryClientProvider client={queryClient}>
-        <Elements stripe={stripePromise}>
-          <Component {...pageProps} />
-        </Elements>
-      </QueryClientProvider>
+      <ApolloProvider client={apolloClient}>
+        <QueryClientProvider client={queryClient}>
+          <Elements stripe={stripePromise}>
+            <Component {...pageProps} />
+          </Elements>
+        </QueryClientProvider>
+      </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withApollo(MyApp);
