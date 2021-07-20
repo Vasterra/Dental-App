fe3d1c9d44c72c684e2278f21e4e3aa236fde4b6
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-07-20 10:44:47 +0300

    Users

--- components/dashboard/totalSubscriptions/graph.tsx
+++ components/dashboard/totalSubscriptions/graph.tsx
@@ -1,65 +1,92 @@
-import React from "react";
+import React, {Component, useEffect, useState} from 'react';
+import {Line} from 'react-chartjs-2';
+import ApiManager from 'services/ApiManager';
 
-const Graph = ({}) => {
-  return (
-    <div className="profile-block-box">
-      <div className="stripes">
-        <div className="total">
-          <p>900</p>
-          <p>800</p>
-          <p>700</p>
-          <p>600</p>
-          <p>500</p>
-          <p>400</p>
-          <p>300</p>
-          <p>200</p>
-          <p>100</p>
-          <p>0</p>
-        </div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="vertical-stripe"></div>
-        <div className="years-block">
-          <p className="year">2021</p>
-          <p className="year-arrows">
-            <img src="../../../images/arrow_left_big.svg" alt="arrow left"/>
-            <img src="../../../images/arrow_right_big.svg" alt="arrow right"/>
-          </p>
-          <p className="circle-gray"></p>
-          <p className="year-text">
-            Free Accounts
-          </p>
-          <p className="circle-gray"></p>
-          <p className="year-text">
-            Subscriptions
-          </p>
+const MONTHS: any[string] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+class Graph extends Component {
+
+  state: any = {
+    dentists: [],
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+    data: {
+      labels: MONTHS,
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          fill: false,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgba(255, 99, 132, 0.2)',
+        },
+      ],
+    }
+  }
+
+  componentDidMount() {
+    this.getListDentists();
+
+  }
+
+  async getListDentists() {
+    ApiManager.getListDentists().then(listDentitst => {
+      this.setState({dentists: listDentitst})
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="profile-block-box">
+
+        <div className="stripes">
+          <div className="charts-block">
+            <Line data={this.state.data} options={this.state.options}/>
+          </div>
+
+          <div className="years-block">
+            <p className="year">2021</p>
+            <p className="year-arrows">
+              <img src="../../../images/arrow_left_big.svg" alt="arrow left"/>
+              <img src="../../../images/arrow_right_big.svg" alt="arrow right"/>
+            </p>
+            <p className="circle-gray"></p>
+            <p className="year-text">
+              Free Accounts
+            </p>
+            <p className="circle-gray"></p>
+            <p className="year-text">
+              Subscriptions
+            </p>
+          </div>
         </div>
       </div>
-      <div className="hor-stripe">
-        <p>January</p>
-        <p>February</p>
-        <p>March</p>
-        <p>April</p>
-        <p>May</p>
-        <p>June</p>
-        <p>July</p>
-        <p>August</p>
-        <p>September</p>
-        <p>October</p>
-        <p>November</p>
-        <p>December</p>
-      </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 export default Graph;
--- pages/admin/users/index.tsx
+++ pages/admin/users/index.tsx
@@ -1,354 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menu from "components/menu";
+import ApiManager from 'services/ApiManager';
 
-const AdminUsers = () => {
+class AdminUsers extends Component {
 
-  return (
-    <section className='container-profile'>
-      <Menu active="Users"/>
-      <div className='main-profile bg-white '>
-        <div className='profile-box-form'>
-          <div className='form-info-block'>
-            <div>
-              <p className='form-login-title green px20'>Users Catalogue</p>
-              <p className='form-login-subtitle gray px12 mb-6px'>Search Users</p>
-            </div>
-          </div>
-          <div className='search'>
-            <input className='search-users' type='search' id='search' name='search' value=''
-                   placeholder=' Search users' />
-            <img className='search-button' src='../../../images/search.svg' alt='search' />
-          </div>
+  state: any = {
+    dentists: [],
+  }
 
-          <div className='user-list-header'>
-            <div className='form-profile-label'>Dentist</div>
-            <div className='form-profile-label select-area' id='account_opened'>Account Opened
-              <ul className='account_opened'>
-                <li>Last Week</li>
-                <li>Last Month</li>
-                <li>Last 3 Months</li>
-                <li>Last 6 Months</li>
-                <li>Last Year</li>
-              </ul>
-            </div>
-            <div className='form-profile-label select-area' id='status'>Status
-              <ul className='status'>
-                <li>Paid</li>
-                <li>Free</li>
-              </ul>
-            </div>
-            <div className='form-profile-label'></div>
-            <div className='form-profile-label'>
-              <img className='pl-13' src='../../../images/arrow-bottom.svg' alt='arrow bottom' />
-            </div>
-          </div>
+  componentDidMount() {
+    this.getListDentists();
 
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' />
-                <span>View Profile</span>
-              </p>
-              <p>
-                <img className='open-user-profile open-info' src='../../../images/plus.svg' />
-              </p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
+  }
+
+  async getListDentists() {
+    ApiManager.getListDentists().then(listDentitst => {
+      this.setState({dentists: listDentitst})
+    });
+  };
+
+
+  clickRowOpen = (id: number) => {
+    const allRow = document.getElementsByClassName('user-info');
+    if (allRow[id].style.display === 'none') {
+      allRow[id].style.display = 'block';
+    } else {
+      allRow[id].style.display = 'none';
+    }
+  };
+
+  render() {
+    console.log(this.state.dentists)
+    return (
+      <section className='container-profile'>
+        <Menu active="Users"/>
+        <div className='main-profile bg-white '>
+          <div className='profile-box-form'>
+            <div className='form-info-block'>
+              <div>
+                <p className='form-login-title green px20'>Users Catalogue</p>
+                <p className='form-login-subtitle gray px12 mb-6px'>Search Users</p>
               </div>
             </div>
-          </div>
+            <div className='search'>
+              <input className='search-users' type='search' id='search' name='search' value=''
+                     placeholder=' Search users'/>
+              <img className='search-button' src='../../../images/search.svg' alt='search'/>
+            </div>
 
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
+            <div className='user-list-header'>
+              <div className='form-profile-label'>Dentist</div>
+              <div className='form-profile-label select-area' id='account_opened'>Account Opened
+                <ul className='account_opened'>
+                  <li>Last Week</li>
+                  <li>Last Month</li>
+                  <li>Last 3 Months</li>
+                  <li>Last 6 Months</li>
+                  <li>Last Year</li>
+                </ul>
+              </div>
+              <div className='form-profile-label select-area' id='status'>Status
+                <ul className='status'>
+                  <li>Paid</li>
+                  <li>Free</li>
+                </ul>
+              </div>
+              <div className='form-profile-label'></div>
+              <div className='form-profile-label'>
+                <img className='pl-13' src='../../../images/arrow-bottom.svg' alt='arrow bottom'/>
               </div>
             </div>
-          </div>
 
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <div className='user-block'>
-            <div className='user-list user-list-text bg-white user-data'>
-              <p>Name</p>
-              <p>01.01.2021</p>
-              <p>Paid Subscription Ends: 03/09/2021</p>
-              <p className='row'><img src='../../../images/user.svg' /> <span>View Profile</span></p>
-              <p><img className='open-user-profile open-info' src='../../../images/plus.svg' /></p>
-            </div>
-            <div className='user-info-text  bg-green user-info'>
-              <div className='flex-2-column'>
-                <div>
-                  <p><strong>Account Email:</strong></p>
-                  <p><strong>GDC Number:</strong></p>
-                  <p><strong>Post Code:</strong></p>
-                  <p><strong>Last Logged In:</strong></p>
-                  <p><strong>Subscription #:</strong></p>
-                  <button className='button-green-outline border-white'>Suspend</button>
-                </div>
-                <div>
-                  <p>Email</p>
-                  <p>12345678</p>
-                  <p>Post Code</p>
-                  <p>13:05 04/04/2021</p>
-                  <p>#StripeID</p>
-                  <button className='button-green-outline border-white'>Delete</button>
-                </div>
-              </div>
-            </div>
+            {
+              this.state.dentists.map((item: any, key: any) => {
+                return (<div className='user-block'>
+                  <div className='user-list user-list-text bg-white user-data'>
+                    <p>{item.firstName}</p>
+                    <p>{item.createdAt}</p>
+                    <p>Paid Subscription Ends: 03/09/2021</p>
+                    <a className='row'>
+                      <img src='../../../images/user.svg'/>
+                      <span>View Profile</span>
+                    </a>
+                    <p>
+                      <img className='open-user-profile open-info' src='../../../images/plus.svg'
+                           onClick={() => this.clickRowOpen(key)}/>
+                    </p>
+                  </div>
+                  <div className='user-info-text bg-green user-info'>
+                    <div className='flex-2-column'>
+                      <div>
+                        <p><strong>Account Email:</strong></p>
+                        <p><strong>GDC Number:</strong></p>
+                        <p><strong>Post Code:</strong></p>
+                        <p><strong>Last Logged In:</strong></p>
+                        <p><strong>Subscription #:</strong></p>
+                        <button className='button-green-outline border-white'>Suspend</button>
+                      </div>
+                      <div>
+                        <p>{item.email ? item.email : 'none'}</p>
+                        <p>12345678</p>
+                        <p>{item.postIndex ? item.postIndex : 'none'}</p>
+                        <p>13:05 04/04/2021</p>
+                        <p>#StripeID</p>
+                        <button className='button-green-outline border-white'>Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>)
+              })
+            }
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 };
 
 export default AdminUsers;
