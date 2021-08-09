3ef6c0ef40b1cdd6a6ec3223902edc80a3acd07f
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-08-09 17:51:59 +0300

    Admin Dentist App: Delete services

--- src/components/settings/services.tsx
+++ src/components/settings/services.tsx
@@ -1,11 +1,9 @@
-import React, {useState} from "react";
-import {AmplifyAuthenticator} from '@aws-amplify/ui-react';
-import Dashboard from 'src/pages/admin/dashboard';
-import {listServiceForDentals} from "src/graphql/queries";
-import {API} from "aws-amplify";
-import {createServiceForDental, deleteServiceForDental, updateServiceForDental} from "src/graphql/mutations";
-import Close from "@material-ui/icons/Close";
-import {CircularProgress} from "@material-ui/core";
+import React, { useState } from 'react';
+import { listServiceForDentals } from 'src/graphql/queries';
+import { API } from 'aws-amplify';
+import { createServiceForDental, deleteServiceForDental, updateServiceForDental } from 'src/graphql/mutations';
+import Close from '@material-ui/icons/Close';
+import { CircularProgress } from '@material-ui/core';
 
 
 const Services = () => {
@@ -15,137 +13,137 @@ const Services = () => {
   const [updateServiceName, setUpdateServiceName]: any = useState();
 
   React.useEffect(() => {
-    getListServiceForDentals()
+    getListServiceForDentals();
   }, []);
 
 
   const getListServiceForDentals = async () => {
-    const arr: any[] = []
-    setServices(null)
-    const {data}: any = await API.graphql({
+    const arr: any[] = [];
+    setServices(null);
+    const { data }: any = await API.graphql({
       query: listServiceForDentals,
       // @ts-ignore
       authMode: 'AWS_IAM'
     });
     data.listServiceForDentals.items.map((item: any, key: any) => {
-      arr.push(item.name)
-    })
-    setUpdateServiceName(arr)
-    setServices(data.listServiceForDentals.items)
-  }
+      arr.push(item.name);
+    });
+    setUpdateServiceName(arr);
+    setServices(data.listServiceForDentals.items);
+  };
 
   const addServiceForDentals = async () => {
     await API.graphql({
       query: createServiceForDental,
       variables: {
         input: {
-          name: service,
+          name: service
         }
       },
       // @ts-ignore
       authMode: 'AWS_IAM'
-    })
+    });
     getListServiceForDentals();
-  }
+  };
 
   const updateServiceForDentals = async (key: any, id: any) => {
-    const updateInput: any = document.getElementsByClassName('form-update-input')
+    const updateInput: any = document.getElementsByClassName('form-update-input');
     if (updateInput[key].disabled) {
-      updateInput[key].disabled = false
-      updateInput[key].style.background = 'white'
+      updateInput[key].disabled = false;
+      updateInput[key].style.background = 'white';
 
     } else {
-      updateInput[key].disabled = true
-      updateInput[key].style.background = 'none'
+      updateInput[key].disabled = true;
+      updateInput[key].style.background = 'none';
       await API.graphql({
         query: updateServiceForDental,
         variables: {
           input: {
             id,
-            name: updateServiceName[key],
+            name: updateServiceName[key]
           }
         },
         // @ts-ignore
         authMode: 'AWS_IAM'
-      })
+      });
       getListServiceForDentals();
     }
+  };
 
-
-  }
+  const deleteService = async (item: { id: any; }) => {
+    await API.graphql({
+      query: deleteServiceForDental,
+      variables: {
+        input: {
+          id: item.id
+        }
+      },
+      // @ts-ignore
+      authMode: 'AWS_IAM'
+    });
+    getListServiceForDentals();
+  };
 
   const onChnage = (e: any, key: any) => {
     const updateInput: any = document.getElementsByClassName('form-update-input');
-    updateInput[key].value = e
-    setUpdateServiceName({[key]: e})
-  }
+    updateInput[key].value = e;
+    setUpdateServiceName({ [key]: e });
+  };
 
   return (
-    <div className="profile-box-form">
-      {!services && <div className="flex-wrapper"><CircularProgress size={120}/></div>}
+    <div className='profile-box-form'>
+      {!services && <div className='flex-wrapper'><CircularProgress size={120} /></div>}
       {services &&
       <>
-          <div className="form-info-block">
-              <div>
-                  <p className="form-login-title green px20">Services Provided</p>
-                  <p className="form-login-subtitle gray px12 ">Available Service Categories</p>
-              </div>
+        <div className='form-info-block'>
+          <div>
+            <p className='form-login-title green px20'>Services Provided</p>
+            <p className='form-login-subtitle gray px12 '>Available Service Categories</p>
           </div>
-          <div className="box-2-box">
-              <div className="profile-block-box">
-                  <div>
-                      <p className="form-profile-label">
-                          <label className="form-profile-label">Add Service</label>
-                      </p>
-                      <p>
-                          <input className="form-profile-input"
-                                 type="text"
-                                 name="add_service"
-                                 id="add_service"
-                                 value={service}
-                                 onChange={(e: any) => setService(e.target.value)}
-                                 placeholder="Service Name Here"/>
-                      </p>
-                  </div>
-                  <p className="row-content">
-                      <button className="button-green" onClick={addServiceForDentals}>Add service</button>
-                  </p>
-              </div>
-              <div className="profile-block-box">
-                  <div>
-                    {services.map((item: any, key: any) => {
-                      return (
-                        <div key={key}>
-                          <input type="text"
-                                 className="form-update-input"
-                                 name="update_service"
-                                 value={updateServiceName[key]}
-                                 id="update_service"
-                                 disabled
-                                 onChange={(e: any) => onChnage(e.target.value, key)}/>
-                          <span onClick={() => updateServiceForDentals(key, item.id)}>
-                    <img className="form-login-input-edit" src="../../../images/edit.svg"/>
+        </div>
+        <div className='box-2-box'>
+          <div className='profile-block-box'>
+            <div>
+              <p className='form-profile-label'>
+                <label className='form-profile-label'>Add Service</label>
+              </p>
+              <p>
+                <input className='form-profile-input'
+                       type='text'
+                       name='add_service'
+                       id='add_service'
+                       value={service}
+                       onChange={(e: any) => setService(e.target.value)}
+                       placeholder='Service Name Here' />
+              </p>
+            </div>
+            <p className='row-content'>
+              <button className='button-green' onClick={addServiceForDentals}>Add service</button>
+            </p>
+          </div>
+          <div className='profile-block-box'>
+            <div>
+              {services.map((item: any, key: any) => {
+                return (
+                  <div key={key}>
+                    <input type='text'
+                           className='form-update-input'
+                           name='update_service'
+                           value={updateServiceName[key]}
+                           id='update_service'
+                           disabled
+                           onChange={(e: any) => onChnage(e.target.value, key)} />
+                    <span onClick={() => updateServiceForDentals(key, item.id)}>
+                    <img className='form-login-input-edit' src='../../../images/edit.svg' />
                   </span>
-                          <Close className="form-login-input-close" onClick={async () => {
-                            await API.graphql({
-                              query: deleteServiceForDental,
-                              variables: {
-                                input: {
-                                  id: item.id
-                                }
-                              },
-                              // @ts-ignore
-                              authMode: 'AWS_IAM'
-                            })
-                            getListServiceForDentals();
-                          }}/>
-                        </div>
-                      )
-                    })
-                    }
+                    <Close className='form-login-input-close' onClick={() => deleteService(item)} />
                   </div>
-              </div>
+                );
+              })
+              }
+            </div>
           </div>
+        </div>
       </>
       }
     </div>
