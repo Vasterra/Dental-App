7befdcbf187873dff2c3fddf8db472db16ee7a2e
ArnoAndAivengo <alexobukhov@hotmail.com>
2021-08-17 11:29:03 +0300

    FrontEnd: Watermark

--- src/components/Dentist/Profile/settings/AddWatermark.tsx
+++ src/components/Dentist/Profile/settings/AddWatermark.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import ReactImageProcess from 'react-image-process';
 import { Storage } from 'aws-amplify';
 import Snackbar from '../../../Snackbar';
 import ApiManager from '../../../../services/ApiManager';
+import watermark from '../../../../utils/watermark/builded';
 
 type Props = {
   currentDentist: any
@@ -15,12 +15,14 @@ const AddWatermark: React.FunctionComponent<Props> = ({ currentDentist }) => {
   const [statusSnackBar, setStatusSnackBar]: any = useState();
   const [openSnackBar, setOpenSnackBar]: any = useState();
   const [currentCover, setCurrentCover]: any = useState();
+  const [currentCoverImg, setCurrentCoverImg]: any = useState();
 
   useEffect(() => {
     downloadCover();
-  }, [])
+  }, []);
 
   const addImage = async (e: any) => {
+    // setCurrentCoverImg(null);
     e.preventDefault();
     let files: any;
     if (e.dataTransfer) {
@@ -40,7 +42,7 @@ const AddWatermark: React.FunctionComponent<Props> = ({ currentDentist }) => {
 
   const uploadCover = async (files: any[]) => {
     try {
-      await Storage.put(`cover/${currentDentist.id}/cover`, files[0], {
+      await Storage.put(`cover/${currentDentist.id}/cover`, files[0] ? files[0] : files, {
         contentType: 'image/png'
       }).then(() => {
         setMessageSnackBar('Success Upload!');
@@ -61,11 +63,12 @@ const AddWatermark: React.FunctionComponent<Props> = ({ currentDentist }) => {
 
   const downloadCover = async () => {
     await ApiManager.downloadCover(currentDentist).then(signedFiles => {
-      setCurrentCover(signedFiles[0])
-    })
+      setCurrentCover(signedFiles[0]);
+    });
   };
 
   const addWatermark = (e: any) => {
+    // setCurrentCoverImg(null);
     e.preventDefault();
     let files;
     if (e.dataTransfer) {
@@ -77,6 +80,7 @@ const AddWatermark: React.FunctionComponent<Props> = ({ currentDentist }) => {
     const reader = new FileReader();
     reader.onload = () => {
       setWatermarkImg(reader.result as any);
+      addWaterMark(reader.result);
     };
     reader.readAsDataURL(files[0]);
   };
@@ -85,48 +89,75 @@ const AddWatermark: React.FunctionComponent<Props> = ({ currentDentist }) => {
     setOpenSnackBar(false);
   };
 
-  console.log(currentCover);
+  const addWaterMark = (e: string | ArrayBuffer | null) => {
+    watermark([currentCover, e])
+    .blob(watermark.image.upperRight())
+    .then((img: any) => {
+      uploadCover(img);
+    });
+
+    watermark([currentCover, e])
+    .image(watermark.image.upperRight())
+    .then((img: any) => {
+      setCurrentCover(null);
+      setCurrentCoverImg(img);
+      document.getElementById('exp')!.appendChild(img);
+    });
+  };
+
   return (
     <>
       <div className='mt-30'>
         <p className='form-profile-label'>
           <label className='form-profile-label'>Cover</label>
         </p>
-        { currentCover &&<p className='profile-photo-box'>
-           <img className='image' src={currentCover} alt='' />
-        </p> }
-        { watermarkImg && <p className='profile-photo-box'>
-          <ReactImageProcess
-            mode='waterMark'
-            waterMarkType='image'
-            waterMark={watermarkImg}
-            width={60}
-            height={60}
-            opacity={0.7}
-            coordinate={[10, 10]}
-          >
-            <img className='image'
-                 src={currentCover}
-                 alt="waterMark"
-            />
-          </ReactImageProcess>
-        </p> }
+        <p className='profile-photo-box'>
+          {currentCover ? <img className='image' src={currentCover} alt='' /> :
+            <div className='image' id='exp' />}
+        </p>
         <p className='row-content'>
-          <label className='button-green-file'>Upload Cover</label>
-          <input type='file' className='input-file' name='watermark' id='watermark'
-                 onChange={addImage} disabled={currentDentist.hasPaidPlan}/>
+          {!currentCover && !watermarkImg && <>
+            <label className='button-green-file'>Upload Cover</label>
+            <input type='file' className='input-file' name='watermark'
+                   onChange={addImage} disabled={currentDentist.hasPaidPlan} />
+          </>}
+          {currentCover && <>
+            <button className='button-green'
+                    disabled={currentDentist.hasPaidPlan}
+                    onClick={() => {
+                      setCurrentCover(null);
+                      setWatermarkImg(null);
+                    }}
+            >
+              Delete Cover
+            </button>
+          </>}
         </p>
       </div>
       <div>
         <p className='form-profile-label'>
           <label className='form-profile-label'>Watermark</label>
         </p>
-         <p className='profile-photo-box'>
-           {watermarkImg &&<img className='image' src={watermarkImg} alt='' />}
+        <p className='profile-photo-box'>
+          {watermarkImg && <img className='image' src={watermarkImg} alt='' />}
         </p>
         <p className='row-content'>
-          <label className='button-green-file'>Upload Watermark</label>
-          <input type='file' className='input-file' name='cover_image' id='cover_image' onChange={addWatermark} disabled={!currentDentist.hasPaidPlan}/>
+          {currentCover && !watermarkImg && <>
+            <label className='button-green-file'>Upload Watermark</label>
+            <input type='file' className='input-file' name='cover_image' id='cover_image' onChange={addWatermark}
+                   disabled={currentDentist.hasPaidPlan} />
+          </>}
+          {currentCoverImg && watermarkImg && <>
+            <button className='button-green'
+                    disabled={currentDentist.hasPaidPlan}
+                    onClick={() => {
+                      setCurrentCover(null);
+                      setWatermarkImg(null);
+                    }}
+            >
+              Clear All
+            </button>
+          </>}
         </p>
       </div>
       <Snackbar
