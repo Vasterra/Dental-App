ec58dd2096a0d7e1d15659556db2975a3a5056a2
itaipov <itaipov97@gmail.com>
2021-08-19 14:11:48 +0600

    div removed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,7 @@ const Layout: React.FunctionComponent<Props> = ({
           currentDentist={currentDentist}
           active={active}
         />
-        <div style={{ minWidth: '333px' }}/>
+        {/*<div style={{ minWidth: '333px' }}/>*/}
         {children}
       </section>
     </Container>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import '../styles/style.css';
 import config from '../../aws-exports';
 import Amplify from 'aws-amplify';
 
