94b6c3d4e16255d12f4c61950aa6bfd77f1849b7
Vasterra <hello@vasterra.com>
2021-09-02 16:40:38 +0500

    payment page footer and more

--- src/pages/dentist/payment/[...slug].tsx
+++ src/pages/dentist/payment/[...slug].tsx
@@ -15,7 +15,7 @@ import {
   useElements,
   useStripe
 } from "@stripe/react-stripe-js";
-import styled from 'styled-components'
+import Footer from 'src/components/Footer';
 
 function Alert(props: AlertProps) {
   return <MuiAlert elevation={6} variant='filled' {...props} />;
@@ -23,218 +23,16 @@ function Alert(props: AlertProps) {
 
 const PaymentContent = ()=>{
 
-  const StyledBox = styled.div`
-    * {
-    box-sizing: border-box;
-    }
-
-  input,
-  button {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    outline: none;
-    border-style: none;
-  }
-
-  #root {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .AppWrapper {
-    width: 500px;
-    height: 400px;
-    position: relative;
-  }
-
-  @keyframes fade {
-    from {
-      opacity: 0;
-      transform: scale3D(0.95, 0.95, 0.95);
-    }
-    to {
-      opacity: 1;
-      transform: scale3D(1, 1, 1);
-    }
-  }
-
-  .Form {
-    animation: fade 200ms ease-out;
-  }
-
-  .FormGroup {
-    margin: 0 15px 20px;
-    padding: 0;
-    border-style: none;
-    background-color: #7795f8;
-    will-change: opacity, transform;
-    box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
-      inset 0 1px 0 #829fff;
-    border-radius: 4px;
-  }
-
-  .FormRow {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-align: center;
-    align-items: center;
-    margin-left: 15px;
-    border-top: 1px solid #819efc;
-  }
-
-  .FormRow:first-child {
-    border-top: none;
-  }
-
-  .FormRowLabel {
-    width: 15%;
-    min-width: 70px;
-    padding: 11px 0;
-    color: #c4f0ff;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-
-  @keyframes void-animation-out {
-    0%,
-    to {
-      opacity: 1;
-    }
-  }
-  .FormRowInput:-webkit-autofill {
-    -webkit-text-fill-color: #fce883;
-    /* Hack to hide the default webkit autofill */
-    transition: background-color 100000000s;
-    animation: 1ms void-animation-out;
-  }
-
-  .FormRowInput {
-    font-size: 16px;
-    width: 100%;
-    padding: 11px 15px 11px 0;
-    color: #fff;
-    background-color: transparent;
-    animation: 1ms void-animation-out;
-  }
-
-  .FormRowInput::placeholder {
-    color: #87bbfd;
-  }
-
-  .StripeElement--webkit-autofill {
-    background: transparent !important;
-  }
-
-  .StripeElement {
-    width: 100%;
-    padding: 11px 15px 11px 0;
-  }
-
-  .SubmitButton {
-    display: block;
-    font-size: 16px;
-    width: calc(100% - 30px);
-    height: 40px;
-    margin: 40px 15px 0;
-    background-color: #f6a4eb;
-    box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
-      inset 0 1px 0 #ffb9f6;
-    border-radius: 4px;
-    color: #fff;
-    font-weight: 600;
-    cursor: pointer;
-    transition: all 100ms ease-in-out;
-    will-change: transform, background-color, box-shadow;
-  }
-
-  .SubmitButton:active {
-    background-color: #d782d9;
-    box-shadow: 0 6px 9px rgba(50, 50, 93, 0.06), 0 2px 5px rgba(0, 0, 0, 0.08),
-      inset 0 1px 0 #e298d8;
-    transform: scale(0.99);
-  }
-
-  .SubmitButton.SubmitButton--error {
-    transform: translateY(15px);
-  }
-  .SubmitButton.SubmitButton--error:active {
-    transform: scale(0.99) translateY(15px);
-  }
-
-  .SubmitButton:disabled {
-    opacity: 0.5;
-    cursor: default;
-    background-color: #7795f8;
-    box-shadow: none;
-  }
-
-  .ErrorMessage {
-    color: #fff;
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    padding: 0 15px;
-    font-size: 13px;
-    margin-top: 0px;
-    width: 100%;
-    transform: translateY(-15px);
-    opacity: 0;
-    animation: fade 150ms ease-out;
-    animation-delay: 50ms;
-    animation-fill-mode: forwards;
-    will-change: opacity, transform;
-  }
-
-  .ErrorMessage svg {
-    margin-right: 10px;
-  }
-
-  .Result {
-    margin-top: 50px;
-    text-align: center;
-    animation: fade 200ms ease-out;
-  }
-
-  .ResultTitle {
-    color: #fff;
-    font-weight: 500;
-    margin-bottom: 8px;
-    font-size: 17px;
-    text-align: center;
-  }
-
-  .ResultMessage {
-    color: #9cdbff;
-    font-size: 14px;
-    font-weight: 400;
-    margin-bottom: 25px;
-    line-height: 1.6em;
-    text-align: center;
-  }
-
-  .ResetButton {
-    border: 0;
-    cursor: pointer;
-    background: transparent;
-  }
-
-  `
     const stripePromise = loadStripe(
       'pk_test_51J15W0B5Yj7B7VjGcyWF6fMvy3UkvUUS5l6YJ3LQqLGFGZgK7UwNyVHLMMVi2HgDweAsAUxkhuukQBjWlTshTPmu00NmYIp1nd'
     );
 
   return (
-    <StyledBox>
-      <div className="AppWrapper">
-        <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
-          <CheckoutForm />
-        </Elements>
-      </div>
-    </StyledBox>
+    <div className="AppWrapper">
+      <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
+        <CheckoutForm />
+      </Elements>
+    </div>
   )
 }
 
@@ -242,17 +40,17 @@ const CARD_OPTIONS = {
   iconStyle: "solid",
   style: {
     base: {
-      iconColor: "#c4f0ff",
-      color: "#fff",
+      iconColor: "#000",
+      color: "#000",
       fontWeight: 500,
       fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
       fontSize: "16px",
       fontSmoothing: "antialiased",
       ":-webkit-autofill": {
-        color: "#fce883"
+        color: "#000"
       },
       "::placeholder": {
-        color: "#87bbfd"
+        color: "#000"
       }
     },
     invalid: {
@@ -544,9 +342,25 @@ const Payment = ({ dentist }: any) => {
       {currentDentist && signedInUser &&
       <Layout title='Account' active={'activePayment'} currentAvatar={currentAvatar} currentDentist={currentDentist}>
        {/* ---------------------------------Change style this PaymentContent--------------------------------------  */}
-        <div className='main-profile' style={{backgroundColor: 'blueviolet', alignItems: 'center', display: 'flex', justifyContent: 'center'}}>
-          <PaymentContent/>
+        <div className='main-profile bg-white biling'>
+          <div className='profile-box-form'>
+            <div className='form-info-block'>
+
+              <div>
+                <p className="form-login-title green px20">Billing Information</p>
+                <p className="form-login-subtitle gray px12 mb-6px"></p>
+              </div>
+
+              <div className='box-to-box'>
+                <div className='biling-block'>
+                  <PaymentContent/>
+                </div>
+              </div>
+              
+            </div>
+          </div>
         </div>
+        <Footer />
         <Snackbar open={openSnackbar} autoHideDuration={2000} onClose={handleCloseSnackbar}>
           <Alert onClose={handleCloseSnackbar}
             // @ts-ignore
